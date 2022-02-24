f59ff81158393848754f7a49f561e94284e335a9
Marc Raymond A. Serrano <marc.serrano8779@gmail.com>
2022-02-24 13:34:18 -0800

    Update priorityQueue.py

--- priorityQueue.py
+++ priorityQueue.py
@@ -5,15 +5,20 @@
 def heapify(arr, n, i):
     # Find the largest among root, left child and right child
     largest = i
+    # left node index is 2i+1
     l = 2 * i + 1
+    # right node index is 2i+2
     r = 2 * i + 2
-
+    # if the left node is located higher on the heap and the value of the current largest node is less than its left node
     if l < n and arr[i] < arr[l]:
+        # make the largest node the left node instead
         largest = l
-
+    # if the right node is located higher on the heap and the value of the current largest node is less than its right node
     if r < n and arr[largest] < arr[r]:
+        # make the largest node the right node instead
         largest = r
 
+    # recursively heapify until the tree is fixed    
     # Swap and continue heapifying if root is not largest
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
@@ -23,10 +28,13 @@ def heapify(arr, n, i):
 # Function to insert an element into the tree
 def insert(array, newNum):
     size = len(array)
+    # if tree is empty, add node to tree
     if size == 0:
         array.append(newNum)
+    # else add the node to the tree
     else:
         array.append(newNum)
+        # then heapify upwards
         for i in range((size // 2) - 1, -1, -1):
             heapify(array, size, i)
 
@@ -38,11 +46,11 @@ def deleteNode(array, num):
     for i in range(0, size):
         if num == array[i]:
             break
-
+    # at this point 'i' is the index of num; swap the values of num and the last node in the tree
     array[i], array[size - 1] = array[size - 1], array[i]
-
+    # remove the last node in the tree
     array.remove(size - 1)
-
+    # after deletion make sure to fix the tree
     for i in range((len(array) // 2) - 1, -1, -1):
         heapify(array, len(array), i)
 
